94cb4a338c7a9b3ea4c293a5d0363e0127960d12
jude <jude@test.com>
2020-06-08 16:24:07 +0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,31 +3,12 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-//     socket.emit('createEmail', {
-//         to: 'jen@example.com',
-//         text: 'Hey. This is Prasad.'
-//     });
- 
- 
-// socket.on('disconnect', function () {
-//     console.log('Disconnected from server');
-// });
-
-// socket.on('newEmail', function (email) {
-//     console.log('New email', email);
-// });
-
-
-socket.emit('createMessage', {
-    from: 'jewel@example.com',
-    text: 'Hey. This is Ravin its working.'
-});
 });
 
 socket.on('disconnect', function () {
-console.log('Disconnected from server'); 
+  console.log('Disconnected from server'); 
 });
 
 socket.on('newMessage', function (message) {
-console.log('New Message', message);
+  console.log('newMessage', message);
 });
--- server/server.js
+++ server/server.js
@@ -14,28 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    // socket.emit('newEmail',{
-    //     from: 'jude@example.com',
-    //     text: 'How is it going on',
-    //     createdAt: 123
-    // });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
-
-    // socket.on('disconnect', () => {
-    //     console.log('User was disconnected');
-    // });
-
-    socket.emit('newMessage',{
-        from: 'NiluMessage@example.com',
-        text: 'How is it going on',
-        createdAt: 123123
-    });
-
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
